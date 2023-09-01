632b84897408f30ddf78a56d1cd2082efdfd9ae1
Jefferson Xavier <parajotaxis@gmail.com>
2023-09-01 17:17:05 -0300

    add calculomedia

---
+++ calculomedia.js
@@ -0,0 +1,9 @@
+// leia tres notas e calcula media ponderada com pesos 2,3 e 5 respectivamente.
+
+var n1 = 5;
+var n2 = 4;
+var n3 = 7;
+var media_3;
+
+var media_3 = (n1*2 + n2*3 + n3*5)/10;
+console.log(media_3);
