603d0fe195c7c55c5b3faa777960884ed98277b3
Jefferson Xavier <parajotaxis@gmail.com>
2023-09-01 16:26:25 -0300

    add calculosalario js

---
+++ calculosalario.js
@@ -0,0 +1,6 @@
+// Dado salario do funcionario (sal = 5000), calcular reajuste (10%) e imprimir
+let sal = 5000;
+let reajuste = 5000 * 0.1;
+let salatual = sal + reajuste;
+
+console.log(salatual);
