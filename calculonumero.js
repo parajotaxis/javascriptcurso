e3a0d32ab75f9da1000d471be57dc4f4a6e56eab
Jefferson Xavier <parajotaxis@gmail.com>
2023-09-01 16:33:45 -0300

    add calculonumer js

---
+++ calculonumero.js
@@ -0,0 +1,11 @@
+// leia um numero e imprima sucessor e antecessor
+
+var numero = 10;
+var sucessor;
+var antecessor;
+
+sucessor = numero + 1;
+antecessor = numero - 1;
+
+console.log(sucessor);
+console.log(antecessor);
