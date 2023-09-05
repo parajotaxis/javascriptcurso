9bdac7bb372608b6a1e21b27dcc3da42bc7e7d56
Jefferson Xavier <parajotaxis@gmail.com>
2023-09-05 20:02:56 -0300

    add classes js

---
+++ classes.js
@@ -0,0 +1,18 @@
+class Pessoa {
+    constructor(nome, sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    falar(){
+        console.log(this.nome);
+    }
+
+    get nomeCompleto(){
+        console.log("Olá " + this.nome + " " + this.sobrenome);
+    }
+}
+
+p1 = new Pessoa("Fabio","Silva");
+p1.falar();
+p1.nomeCompleto;
